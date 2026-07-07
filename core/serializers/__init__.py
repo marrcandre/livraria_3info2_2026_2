48d5cffc690d834bb07c6cc6d502b010a7a331d5
Marco André Mendes <marcoandre@gmail.com>
2026-07-07 14:29:04 -0300

    feat: criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,15 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer
+from .compra import (
+    CompraSerializer,
+    ItensCompraSerializer,
+    CompraCreateUpdateSerializer,
+    ItensCompraCreateUpdateSerializer,
+)
 from .editora import EditoraSerializer
-from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
+from .livro import (
+    LivroListSerializer,
+    LivroRetrieveSerializer,
+    LivroSerializer,
+)
 from .user import UserRegistrationSerializer, UserSerializer
