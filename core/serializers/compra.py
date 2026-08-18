da9af34061df002abec903cb7180b6b25227a75e
Marco André Mendes <marcoandre@gmail.com>
2026-08-18 14:38:43 -0300

    feat: gravação do preço do livro no item da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -2,7 +2,6 @@ from django.db import transaction
 from rest_framework.serializers import (
     CharField,
     CurrentUserDefault,
-    DecimalField,
     HiddenField,
     ModelSerializer,
     SerializerMethodField,
@@ -19,7 +18,7 @@ from core.models import Compra, ItensCompra
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade')
+        fields = ('livro', 'quantidade', 'preco')
 
     def validate_quantidade(self, quantidade):
         if quantidade <= 0:
@@ -37,24 +36,18 @@ class ItensCompraListSerializer(ModelSerializer):
 
     class Meta:
         model = ItensCompra
-        fields = ('quantidade', 'livro')
+        fields = ('livro', 'quantidade', 'preco')
 
 
 class ItensCompraSerializer(ModelSerializer):
     titulo = CharField(source='livro.titulo', read_only=True)
     editora = CharField(source='livro.editora.nome', read_only=True)
-    preco = DecimalField(
-        source='livro.preco',
-        max_digits=7,
-        decimal_places=2,
-        read_only=True,
-    )
     capa = CharField(source='livro.capa.url', read_only=True)
 
     total = SerializerMethodField()
 
     def get_total(self, item):
-        return item.livro.preco * item.quantidade
+        return item.preco * item.quantidade
 
     class Meta:
         model = ItensCompra
@@ -86,6 +79,7 @@ class CompraCreateUpdateSerializer(ModelSerializer):
         itens_data = validated_data.pop('itens')
         compra = Compra.objects.create(**validated_data)
         for item_data in itens_data:
+            item_data['preco'] = item_data['livro'].preco
             ItensCompra.objects.create(compra=compra, **item_data)
         return compra
 
@@ -95,6 +89,7 @@ class CompraCreateUpdateSerializer(ModelSerializer):
         if itens_data is not None:
             compra.itens.all().delete()
             for item_data in itens_data:
+                item_data['preco'] = item_data['livro'].preco
                 ItensCompra.objects.create(compra=compra, **item_data)
         return super().update(compra, validated_data)
 
